48b2e8b96b495b11593cf65a6e004e3ad514037b
Sebastian Cabrera <okseby@protonmail.com>
2024-03-28 21:46:36 -0400

    Small configuration changes

--- desktop/src/dev/bangbang/loom/DesktopLauncher.java
+++ desktop/src/dev/bangbang/loom/DesktopLauncher.java
@@ -7,7 +7,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1600, 900);
 		config.setForegroundFPS(60);
+		config.setIdleFPS(15);
+		config.useVsync(true);
 		config.setTitle("Loom");
 		new Lwjgl3Application(new Game(), config);
 	}
